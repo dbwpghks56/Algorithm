60bbf6b3da1db1d2687b0bdffbe468247b58c830
JaeHwan <wpghks56@naver.com>
2023-06-08 17:49:35 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 388 ms, Memory: 4900 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <algorithm>
+
+int main(void)
+{
+	int number, data[1000001];
+
+	scanf("%d", &number);
+
+	for ( int i = 0; i < number; i++ )
+	{
+		scanf("%d", &data[i]);
+	}
+
+	std::sort(data, data + number);
+
+	for ( int i = 0; i < number; i++ )
+	{
+		printf("%d\n", data[i]);
+	}
+
+	return 0;
+}
