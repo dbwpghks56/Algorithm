1a8a562f20c315761875f3aa0e735bbb2f114418
JaeHwan <wpghks56@naver.com>
2023-06-08 17:43:02 +0900

    [Bronze I] Title: 팰린드롬수, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
@@ -0,0 +1,12 @@
+import sys
+
+input = sys.stdin.readline
+
+def is_pelin(st):
+    return st == st[::-1]
+
+while True:
+    n = input().rstrip()
+    if n == "0" : break
+    print( "yes" if is_pelin(n) else "no" )
+    
