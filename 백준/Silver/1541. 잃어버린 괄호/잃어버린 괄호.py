dbe44317b85fa0505e5219d1e7566159a8573a8d
JaeHwan <wpghks56@naver.com>
2023-06-08 17:50:25 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+strr = input().split("-")
+answer = 0
+
+def mySum(v):
+    plustResult = 0
+    plus = str(v).split("+")
+    for c in plus:
+        plustResult += int(c)
+        
+    return plustResult
+
+for i in range(len(strr)):
+    pp = mySum(strr[i])
+    
+    if i == 0 :
+        answer += pp
+        
+    else:
+        answer -= pp
+    
+
+print(answer)
