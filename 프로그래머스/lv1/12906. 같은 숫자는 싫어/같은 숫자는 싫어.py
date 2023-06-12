c7d4f621fb6540ddda2a4725eb52821cc8ad9424
JaeHwan <wpghks56@naver.com>
2023-06-12 14:42:02 +0900

    Update 같은 숫자는 싫어.py

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,8 +1,5 @@
-from collections import deque
-
 def solution(arr):
     answer = []
-    asset = deque()
     for a in arr:
         if answer == []:
             answer.append(a)
@@ -10,20 +7,7 @@ def solution(arr):
         else:
             if answer[-1] != a:
                 answer.append(a)
-#         print(asset)
-#         print(answer)
-#         if len(asset) == 0:
-#             asset.append(a)
-            
-#         else:
-#             ast = asset.popleft()
-#             if a == ast:
-#                 answer.append(ast)
-                
-#             else:
-#                 answer.append(ast)
-#                 asset.append(a)
-            
+         
     return answer
 
 
