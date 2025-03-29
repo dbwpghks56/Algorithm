4c4643211448d27d21759ada634cb0d2e61b0bd0
JaeHwan <wpghks56@naver.com>
2025-03-29 11:32:22 +0900

    [Silver V] Title: 너의 평점은, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -0,0 +1,19 @@
+import sys
+
+
+total = {'A+': 4.5, 'A0': 4.0, 'B+' : 3.5, 'B0': 3.0, 'C+':2.5, 'C0':2.0, 'D+':1.5, 'D0':1.0, 'F':0.0}
+
+sum = 0
+sumhak = 0
+countP = 0
+
+for _ in range(20):
+    c, a, b = sys.stdin.readline().split()
+    if b == 'P':
+        countP += 1
+    else:
+        aa = float(a)
+        sumhak += aa
+        sum += aa * total[b]
+
+print(sum / (sumhak))
